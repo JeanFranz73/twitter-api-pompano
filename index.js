1dcf58c3b01f586e5792858b680164744584f9cb
Jean Franz <franzjean37@gmail.com>
2022-07-31 04:48:54 -0300

    Ajustes para redundância em JSON

--- index.js
+++ index.js
@@ -2,63 +2,67 @@ const fs = require('fs');
 const app = require("express")();
 const axios = require("axios");
 const config = require("./config");
-var ultimaData = new Date();
-var ultimoTweet;
+var json = require('./tweet.json')
+
+var ultimoTweet = json.ultimoTweet;
+var ultimaData = new Date(json.ultimaData);
+
 app.set('json spaces', 4);
 
-fs.readFile('./tweet.txt', 'utf8', function (err, data) {
-  if (err) return console.log(err);
-  ultimoTweet = data;
-});
-
 setInterval(() => {
-  run()
+    run()
 }, 1000);
 
 function run() {
-  axios
-    .get(`https://api.twitter.com/2/users/${config.twitter.id}/tweets`, config.http)
-    .then((res) => {
-      if (res.data.meta.newest_id == ultimoTweet) {
-        console.log("ultimo tweet ja postado")
-      } else {
-        webhookPost(res.data.meta.newest_id)
-        ultimoTweet = res.data.meta.newest_id
-        fs.writeFile('./tweet.txt', res.data.meta.newest_id, function (err) {
-          if (err) return console.log(err);
-        });
-      }
-    })
-    .catch((err) => {
-      console.error('erro ao consultar a API do Twitter')
-      console.log(err)
-    })
+    axios
+        .get(`https://api.twitter.com/2/users/${config.twitter.id}/tweets`, config.twitter.http)
+        .then((res) => {
+            if (res.data.meta.newest_id != ultimoTweet) {
+                ultimoTweet = res.data.meta.newest_id
+
+                let data = {
+                    ultimoTweet,
+                    ultimaData: ultimaData
+                }
+
+                webhookPost(ultimoTweet);
+
+                fs.writeFile('./tweet.json', JSON.stringify(data), function (err) {
+                    if (err) return console.log(err);
+                });
+            } else {
+                console.log("ultimo tweet ja postado")
+            }
+        })
+        .catch((err) => {
+            console.error('erro ao consultar a API do Twitter')
+            console.log(err)
+        })
 }
 
 function webhookPost(twitterInfo) {
-  axios
-    .post(config.discord_webhook, {
-      username: "Twitter - Pompano Blog",
-      content: `Ei <@&934929778576879627>, notícia fresquinha! https://twitter.com/pompanoblog/status/${twitterInfo}`
-    })
-    .then((res) => {
-      ultimaData = new Date();
-      console.log(`webhook postado com sucesso`);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    axios
+        .post(config.discord_webhook, {
+            username: "Twitter - Pompano Blog",
+            content: `Ei <@&934929778576879627>, notícia fresquinha! https://twitter.com/pompanoblog/status/${twitterInfo}`
+        })
+        .then((res) => {
+            ultimaData = new Date();
+            console.log(`webhook postado com sucesso`);
+        })
+        .catch((err) => {
+            console.error('Ocorreu um erro ao enviar o webhook');
+        });
 }
 
 app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'ok',
-
-    ultimaPostagemWebhook: ultimaData.getTime(),
-    ultimoTweet
-  })
+    res.status(200).json({
+        status: 'ok',
+        ultimaData: ultimaData.getTime(),
+        ultimoTweet
+    })
 });
 
 app.listen(8000, () => {
-  console.log("ouvindo na porta 8000")
-})
+    console.log("ouvindo na porta 8000")
+});
