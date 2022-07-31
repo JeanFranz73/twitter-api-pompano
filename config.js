4449027383c27958af3a00c460afde63a58cc455
Jean Franz <franzjean37@gmail.com>
2022-07-31 03:03:30 -0300

    Ajustes gitignore e .env

--- config.js
+++ config.js
@@ -2,19 +2,15 @@ require('dotenv').config()
 
 const config = {
     twitter: {
-        consumer_key: process.env.TWITTER_API_KEY,
-        consumer_secret: process.env.TWITTER_API_SECRET,
-        access_token_key: process.env.ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-        bearer_token: process.env.TWITTER_BEARER,
         id: process.env.TWITTER_USER_ID,
+        http: {
+            headers: { 'Authorization': `Bearer ${process.env.TWITTER_BEARER}` },
+            params: {
+                exclude: 'replies,retweets'
+            }
+        },
     },
-    http: {
-        headers: { 'Authorization': `Bearer ${process.env.TWITTER_BEARER}` },
-        params: {
-            exclude: 'replies,retweets'
-        }
-    },
+
     discord_webhook: process.env.DISCORD_WEBHOOK,
 
 }
